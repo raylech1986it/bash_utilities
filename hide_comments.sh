e6b3052b40f742118b059cb8a011dedab7630000
raylech1986it <20132837+raylech1986it@users.noreply.github.com>
2020-11-30 17:35:32 -0600

    Update hide_comments.sh
    There were some bugs in the previous version of 'hide_comments.sh'.
    
    Hence, it will be re-developed from the basic version: hiding commented or new-lines.

--- hide_comments.sh
+++ hide_comments.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-
-# This bash script will remove all commented lines or any lines that start
+# This script will remove all commented lines or any lines that start
 # with a new line character (preceded by 0 or more whitespaces).
+#
 # A whitespace in this script is defined as: [ \r\t\f]
 #
-# This script is great mostly for config files where you want to remove all the "fluff" and see only
-# what the config file is implementing. Side note, the bash script just invokes a Perl one-liner.
+# This script is mostly for config files where you want to remove all the "fluff" and see only
+# the "effective" settings. 
 #
-# The file can be passed to it as an argument:
+# This script basically invokes a Perl one-liner.
+#
+# A file can be passed to it as an argument:
 #
 # ./hideComments.sh  /etc/ssh/sshd_config
 #
@@ -18,18 +20,9 @@
 
 
 # A whitespace in this script is defined as: [ \r\t\f]
-/usr/bin/perl -ne  ' unless ( /^[ \r\t\f]*#|;/ || /^[ \r\t\f]*\n/) { 
+/usr/bin/perl -ne  ' unless ( /^[ \r\t\f]*# || /^[ \r\t\f]*\n/) { 
 
-#color-code common "section bracket line"
-# i.e. hc /etc/samba/smb.conf
-if ( /^\[/ ){ 
- print   "\033[0;32m$_\033[0m" 
-}
-
-else {
  print   "$_" 
-}
-
 
 } ' $1
 
