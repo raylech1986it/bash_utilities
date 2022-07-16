08690be01e6a93357c3c68f8eaf59e1c4b667a9b
raylech1986it <20132837+raylech1986it@users.noreply.github.com>
2022-07-16 12:14:27 -0500

    Created a function in 'createTestgitRepo.sh'
    Created function 'make_commit' in 'createTestgitRepo.sh' to clean up redundant code.

--- createTestgitRepo.sh
+++ createTestgitRepo.sh
@@ -1,48 +1,30 @@
 rm -Rf .git *
 
+make_commit(){
+ echo ":)" >> ./script.sh
+ git add *
+ git commit -m "Commit message"
+ sleep 1
+}
+
+
 git init
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
-sleep 2
 
+make_commit
+make_commit
+make_commit
 
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
-sleep 2
-
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
-sleep 2
-
-git branch dev2
+git branch feature
+git checkout feature
 sleep 1
-git checkout dev2
-sleep 2
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
-sleep 2
 
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
-sleep 2
+make_commit
+make_commit
 
 git checkout master
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
 sleep 2
 
-
-echo ":)" >> ./script.sh
-git add *
-git commit -m "Commit message"
-sleep 2
-
+make_commit
+make_commit
 
 git log --oneline --graph --decorate --all
-
