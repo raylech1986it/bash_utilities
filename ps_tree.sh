8ddb0b23378eb5f2b00d3c9006822ef806eb093b
raylech1986it <20132837+raylech1986it@users.noreply.github.com>
2022-12-16 17:31:35 -0600

    Updated comments in ps_tree.sh
    Updated comments in 'ps_tree.sh' on how to use script.

--- ps_tree.sh
+++ ps_tree.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/sh
 
 # 
-# This script will assume the PID provided as the root and display
-# its information with the command 'ps'.
+# This script will assume the process ID (PID) provided as the root
+# and display its information with the command 'ps'.
 #
 # Plus, all of its child processes.
 #
-
+# Ways to use this command:
+#
+# sh ./ps_tree.sh <PID>
+# ./ps_tree.sh <PID>
+#
 
 ROOT_PID=$1
 
